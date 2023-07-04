f33cb88aff301d77ec12eeffa3bfd5b347946ee8
arvindpndit <arvindpandit9808@gmail.com>
2023-07-04 20:20:43 +0530

    body-parser setup complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
 
 const setupAndStartServer = async () => {
   const app = express();
+
+  app.use(bodyParser);
+  app.use(bodyParser.urlencoded({ extended: true }));
+
   app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
   });
