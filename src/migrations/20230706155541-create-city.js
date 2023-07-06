6824bd427747fda59b6aa0bf55228f8dff71c9a9
arvindpndit <arvindpandit9808@gmail.com>
2023-07-06 21:37:27 +0530

    added "allowNull: false" in two files

--- src/migrations/20230706155541-create-city.js
+++ src/migrations/20230706155541-create-city.js
@@ -1,28 +1,29 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Cities', {
+    await queryInterface.createTable("Cities", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Cities');
-  }
+    await queryInterface.dropTable("Cities");
+  },
 };
--- src/models/city.js
+++ src/models/city.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class City extends Model {
     /**
@@ -13,11 +11,15 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  City.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'City',
-  });
+  City.init(
+    {
+      name: DataTypes.STRING,
+      allowNull: false,
+    },
+    {
+      sequelize,
+      modelName: "City",
+    }
+  );
   return City;
 };
