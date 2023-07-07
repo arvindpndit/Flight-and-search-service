8a1b39516372eb66c60b5f8127c97b9c71b9ac8b
arvindpndit <arvindpandit9808@gmail.com>
2023-07-07 21:26:02 +0530

    added city service

---
+++ src/services/city-service.js
@@ -0,0 +1,52 @@
+const { CityRepository } = require("../repository/city-repository");
+
+class CityService {
+  constructor() {
+    this.CityRepository = new CityRepository();
+  }
+
+  async createCity(cityData) {
+    try {
+      const city = await this.CityRepository.createCity(cityData);
+      return city;
+    } catch (error) {
+      console.log("something went wrong in the service layer");
+      throw { error };
+    }
+  }
+
+  async deleteCity(cityId) {
+    try {
+      const deletedcityResponse = await this.CityRepository.deleteCity(cityId);
+      return deletedcityResponse;
+    } catch (error) {
+      console.log("something went wrong in the service layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = this.CityRepository.getCity(cityId);
+      return city;
+    } catch (error) {
+      console.log("something went wrong in the service layer");
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, cityData) {
+    try {
+      const cityResponse = await this.CityRepository.updateCity(
+        cityId,
+        cityData
+      );
+      return cityResponse;
+    } catch (error) {
+      console.log("something went wrong in the service layer");
+      throw { error };
+    }
+  }
+}
+
+module.exports = CityService;
