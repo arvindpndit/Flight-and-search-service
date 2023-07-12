902b6ebcaf98b75cf867ff307191a8496fbbbb49
arvindpndit <arvindpandit9808@gmail.com>
2023-07-12 10:05:57 +0530

    added add-airports seeder file

---
+++ src/seeders/20230712043101-add-airports.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      "Airports",
+      [
+        {
+          name: "Kempegowda International Airport",
+          cityId: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Mysore Airport",
+          cityId: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Mengaluru International Airport",
+          cityId: 10,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Indira Gandhi International Airport",
+          cityId: 13,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
